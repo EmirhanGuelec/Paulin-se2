7397f9cb6e58f259fc2f9b49cf6738c42f11b644
Emirhan Gülec <emirhanguelec@gmail.com>
2025-06-25 16:55:52 +0200

    Update urls.py
    Like/Kommentare Live änderungen

--- Django-Projekt/meine_app/urls.py
+++ Django-Projekt/meine_app/urls.py
@@ -2,22 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Startseite & Post-Funktionen
     path('', views.startseite, name='startseite'),
     path('post/create/', views.create_post, name='create_post'),
     path('post/<int:post_id>/comment/', views.add_comment, name='add_comment'),
-    path('post/<int:post_id>/like/', views.toggle_like, name='toggle_like'),
-
-
-    # Statische Seiten und Navigation
+    path('post/toggle-like-ajax/', views.toggle_like_ajax, name='toggle_like_ajax'),
+    path('post/add-comment-ajax/', views.add_comment_ajax, name='add_comment_ajax'),
     path('chat/', views.chat, name='chat'),
     path('freunde/', views.freunde, name='freunde'),
     path('impressum/', views.impressum, name='impressum'),
     path('login/', views.login, name='login'),
     path('login_check/', views.login_check, name='login_check'),
     path('posten/', views.posten, name='posten'),
-    path('registrierung/', views.register, name='registrierung'),
+    path('registrierung/', views.registrierung, name='registrierung'),
     path('suche/', views.suche, name='suche'),
     path('einstellungen/', views.einstellungen, name='einstellungen'),
 ]
-
